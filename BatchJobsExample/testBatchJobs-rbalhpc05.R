f7b539b26f089a4255504accd3c61731f9034f49
zhangj83 <jitao_david.zhang@roche.com>
2016-10-20 12:13:47 +0200

    the script's name suggests that it should run on the cluster head

---
+++ BatchJobsExample/testBatchJobs-rbalhpc05.R
@@ -0,0 +1,41 @@
+library(BatchJobs)
+
+## run the script on rbalhpc05!
+
+##----------------------------------------##
+## Map-Reduce using BatchJobs
+##----------------------------------------##
+f <- function(x) {
+  if (x > 4) Sys.sleep(x/10)
+  if (x == 7) stop("Invalid 'x': ", x)
+  -x
+}
+y <- lapply(5:14, FUN=function(x) try(f(x)))
+str(y)
+
+loadConfig()
+
+## setup a BatchJob registration
+
+reg <- makeRegistry(id="HBcalc")
+print(reg)
+ids <- batchMap(reg, fun=f, 5:14)
+print(ids)
+
+## show status of registry
+print(reg)
+showStatus(reg)
+
+done <- submitJobs(reg, resources=list(walltime=3600))
+
+print(done)
+showStatus(reg)
+
+## load result of job #2
+loadResult(reg, 2)
+
+## laod results of all 'done jobs'
+loadResults(reg)
+
+## remove the registry
+BatchJobs::removeRegistry(reg, ask="no")
