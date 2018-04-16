d800965540c7da2d2f92acf152f5828c8f444969
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-04-16 21:27:43 +0200

    larger fonts according to Agnes

--- Agnes-firstBusProgramm-flask/tram.py
+++ Agnes-firstBusProgramm-flask/tram.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-    return "<h1><font color='red'>The First Python Program of Agnes</font></h1><h3>16.04.2018</h3><p style='font-size:60px'>Hello, <bold>Agnes</bold>, I'm tram/bus Nr <span style='color:pink;font-size:60px'>" + str(random.randint(1,46)) + "</span>!<p>"
+    return "<h1><font color='red'>The First Python Program of Agnes</font></h1><h3>16.04.2018</h3><p style='font-size:120px'>Hello, <bold>Agnes</bold>, I'm tram/bus Nr <span style='color:pink;font-size:120px'>" + str(random.randint(1,46)) + "</span>!<p>"
