6f2c312825e9918be4945714d4e75c1b6947d818
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-05-08 13:09:21 +0200

    add page to show all todos

--- 2018-05-intweb/flask-example.py
+++ 2018-05-intweb/flask-example.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import sqlite3
 
 webapp = Flask(__name__)
 
@@ -14,3 +15,9 @@ def user():
     return render_template("user.html", user=user_dict)
 
 	
+@webapp.route('/todo/all', methods=['GET'])
+def show_alla_todos():
+    db = sqlite3.connect("todo.sqlite3")
+    cur = db.execute('SELECT * FROM todoTbl')
+    rows = cur.fetchall()
+    return render_template('todos.html', rows = rows);
