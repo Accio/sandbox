474359a1c18359ca7a7d40ae88fc47e245f67fd9
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-05-11 06:39:08 +0200

    before app request

--- 2018-05-flasktodo/flasktd/auth.py
+++ 2018-05-flasktodo/flasktd/auth.py
@@ -62,3 +62,15 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+# At the beginning of each request, if a user is logged in, their information should be loaded and made available to other views
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+                'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
