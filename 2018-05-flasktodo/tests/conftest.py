1ac19515b7b6fbe2df3afb9f7b58bb84075592dd
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-05-12 14:57:22 +0200

    add AuthActions to simulate login/logout

--- 2018-05-flasktodo/tests/conftest.py
+++ 2018-05-flasktodo/tests/conftest.py
@@ -19,7 +19,7 @@ def app():
 
     with app.app_context():
         init_db()
-        get_db().executescript(_data_sql‘）
+        get_db().executescript(_data_sql)
 
     yield app
 
@@ -33,3 +33,22 @@ def client(app):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+## authentication
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
