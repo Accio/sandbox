1540ff62fba64005a73295d0af07fc5c1f714774
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-05-12 06:21:43 +0200

    query todos

--- 2018-05-flasktodo/flasktd/todo.py
+++ 2018-05-flasktodo/flasktd/todo.py
@@ -8,4 +8,13 @@ from flasktd.db import get_db
 
 bp = Blueprint('todo', __name__)
 
+@bp.route('/')
+def index():
+    db = get_db()
+    todos = db.execute(
+        'SELECT t.id, name, description, created, deadline, user_id, user_name'
+        ' FROM todo t JOIN user u ON t.user_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('todo/index.html', todos=todos)
 
